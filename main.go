f54337770242d7a44dcda9c1258282cb83ce44e1
KyungWonPark <kw.park.person@gmail.com>
2020-09-14 07:12:46 +0900

    sadf"

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 	backend := make([]float64, 13362*13362)
 	afterThreshold := mat.NewSymDense(13362, backend)
 
-	thsArr := []float64{0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95}
+	thsArr := []float64{0}
 
 	for _, threshold := range thsArr {
 		calc.DoThresholding(matBuffer, afterThreshold, threshold)
